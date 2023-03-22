07425a2cca25931252069c96e7fb79012b5116cc
valiantlynx <86688436+valiantlynx@users.noreply.github.com>
2023-03-22 14:40:25 +0100

    Update setup_ec2_server.sh

--- setup_ec2_server.sh
+++ setup_ec2_server.sh
@@ -62,3 +62,34 @@ sleep 3
 #cd /etc/nginx/sites-enabled/
 sudo systemctl status nginx
 echo "Finished set up"
+
+echo "Do you want to clone a github repo (y/n): "
+read want
+
+if [[ $want == "y" || $want == "yes" ]]; then
+    echo "Great, is it public or private? (1-public / 2-private)"
+    read status
+    if [[ $status == "1" || $status == "public" || $status == "1-public" ]]; then
+        echo "You chose public, what is your github account name: "
+        read account
+        echo "what is the repo name: "
+        read repo
+        echo $(git clone https://github.com/$account/$repo.git)
+        echo "we are done"
+    else
+        echo "You chose private, what is your github account name: "
+        read account
+        echo "what is the repo name: "
+        read repo
+        echo "what is your git token, you can get it from(https://github.com/settings/tokens), when making it make sure the "repo" checkbox is checked: "
+        read token
+        echo $(git clone https://$token@github.com/$account/$repo.git)
+        echo "we are done"
+        exit 1   
+    fi
+else
+    echo "You chose no"
+    exit 1
+fi
+
+
