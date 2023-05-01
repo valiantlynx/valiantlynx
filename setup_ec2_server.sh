e04701b443f945d459728c304bf96fd51b2e234a
valiantlynx <86688436+valiantlynx@users.noreply.github.com>
2023-05-01 03:19:37 +0200

    Update setup_ec2_server.sh

--- setup_ec2_server.sh
+++ setup_ec2_server.sh
@@ -22,7 +22,7 @@ sleep 3
 echo "starting docker setup"
 sleep 3
 
- curl -fsSL https://download.docker.com/linux/ubuntu/gpg  |  gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg -y
+ curl -fsSL https://download.docker.com/linux/ubuntu/gpg  |  gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \$(lsb_release -cs) stable" |  tee /etc/apt/sources.list.d/docker.list > /dev/null
 echo "Initialize install docker.io"
 sleep 3
