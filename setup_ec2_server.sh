151b8d510d6d45df3a12a348e2d725c780b27389
valiantlynx <86688436+valiantlynx@users.noreply.github.com>
2023-05-01 03:04:31 +0200

    Update setup_ec2_server.sh

--- setup_ec2_server.sh
+++ setup_ec2_server.sh
@@ -2,65 +2,65 @@
 echo "logged in"
 echo "Update ubuntu"
 sleep 3
-sudo apt-get update
+apt-get update
 echo "Initialize install git"
 sleep 3
-sudo apt-get install git
+ apt-get install git
 echo "Initialize install ca-certificates"
 sleep 3
-#sudo git clone https://github.com/edatut/gunidocker.git
-sudo apt-get install  ca-certificates
+# git clone https://github.com/edatut/gunidocker.git
+ apt-get install  ca-certificates
 echo "Initialize install curl"
 sleep 3
-sudo apt-get install  curl
+ apt-get install  curl
 echo "Initialize install gnupg"
 sleep 3
-sudo apt-get install gnupg
+ apt-get install gnupg
 echo "Initialize install ca-certificates"
 sleep 3
-sudo apt-get install lsb-release
+ apt-get install lsb-release
 echo "starting docker setup"
 sleep 3
 
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg  | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg -y
-echo   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+ curl -fsSL https://download.docker.com/linux/ubuntu/gpg  |  gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg -y
+echo   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \$(lsb_release -cs) stable" |  tee /etc/apt/sources.list.d/docker.list > /dev/null
 echo "Initialize install docker.io"
 sleep 3
 
-sudo apt install docker.io -y
+ apt install docker.io -y
 echo "Initialize install docker compose"
 sleep 3
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+ curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 echo "Initialize execute permissions for docker compose"
 sleep 3
-sudo chmod +x /usr/local/bin/docker-compose
+ chmod +x /usr/local/bin/docker-compose
 echo "starting nginx setup"
 echo "Initialize install nginx "
 sleep 3
 
-sudo apt install nginx -y
+ apt install nginx -y
 echo "Initialize install snapd "
 sleep 3
-sudo apt install snapd -y
+ apt install snapd -y
 echo "Initialize install core "
 sleep 3
-sudo snap install core 
+ snap install core 
 echo "Initialize refresh core "
 sleep 3
-sudo snap refresh core 
+ snap refresh core 
 echo "Initialize install certbot "
 sleep 3
-sudo snap install --classic certbot
+ snap install --classic certbot
 echo "Initialize ln -s /snap/bin/certbot /usr/bin/certbot "
 sleep 3
-sudo ln -s /snap/bin/certbot /usr/bin/certbot
+ ln -s /snap/bin/certbot /usr/bin/certbot
 echo "Initialize certbot certonly --nginx "
 sleep 3
-sudo certbot certonly --nginx
+ certbot certonly --nginx
 echo "Checking nginx status install docker "
 sleep 3
 #cd /etc/nginx/sites-enabled/
-sudo systemctl status nginx
+ systemctl status nginx
 echo "Finished set up"
 
 echo "Do you want to clone a github repo (y/n): "
