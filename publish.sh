5761ed9bc540881a4cfbc8dc904e803f97844312
valiantlynx <86688436+valiantlynx@users.noreply.github.com>
2023-05-20 15:11:10 +0200

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -1,22 +1,22 @@
-git clone https://github.com/valiantlynx/altlokalt-nextjs.git
+git clone https://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@github.com/valiantlynx/altlokalt-nextjs.git
 echo "Cloned altlokalt-nextjs"
-git clone https://github.com/valiantlynx/anime-site.git
+git clone https://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@github.com/valiantlynx/anime-site.git
 echo "Cloned anime-site"
-git clone https://github.com/valiantlynx/pocketbase-upload-maga.git
+git clone https://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@github.com/valiantlynx/pocketbase-upload-maga.git
 echo "Cloned pocketbase-upload-maga"
-git clone https://github.com/valiantlynx/manga-site.git
+git clone https://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@github.com/valiantlynx/manga-site.git
 echo "Cloned manga-site"
-git clone https://github.com/valiantlynx/simple-service-worker.git
+git clone https://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@github.com/valiantlynx/simple-service-worker.git
 echo "Cloned simple-service-worker"
-git clone https://github.com/valiantlynx/blog-nextjs-sanity.git
+git clone https://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@github.com/valiantlynx/blog-nextjs-sanity.git
 echo "Cloned blog-nextjs-sanity"
-git clone https://github.com/valiantlynx/minfuel-web.git
+git clone https://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@github.com/valiantlynx/minfuel-web.git
 echo "Cloned minfuel-web"
-git clone https://github.com/valiantlynx/animevariant_com_api.git
+git clone https://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@github.com/valiantlynx/animevariant_com_api.git
 echo "Cloned animevariant_com_api"
-git clone https://github.com/valiantlynx/next-blog.git
+git clone https://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@github.com/valiantlynx/next-blog.git
 echo "Cloned next-blog"
-git clone https://github.com/valiantlynx/pocketbase-docker.git
+git clone https://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@github.com/valiantlynx/pocketbase-docker.git
 echo "Cloned altlokalt-nextjs"
 
 # compose
