05ea05d87a51d81d696e37e1c7060f4b8a28023e
valiantlynx <gormerykombo@gmail.com>
2023-06-28 18:15:56 +0200

    gcloud

---
+++ setup_gcloud.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt-get update
+curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-437.0.0-linux-x86_64.tar.gz
+
+# Extract the contents of the file to any location on your file system (preferably your Home directory). To replace an existing installation, remove the existing google-cloud-sdk directory and then extract the archive to the same location.:
+tar -xf google-cloud-cli-437.0.0-linux-x86_64.tar.gz
+./google-cloud-sdk/install.sh
+
+# restart shell
+exec -l $SHELL
+
+./google-cloud-sdk/bin/gcloud init
