a2394958db97d33d9525ed8cae9cece19f8b279b
valiantlynx <gormerykombo@gmail.com>
2023-06-30 16:06:07 +0200

    dfx

--- install_dfx_linux_deb.sh
+++ install_dfx_linux_deb.sh
@@ -3,11 +3,11 @@
 DFX_VERSION=0.9.3 sh -ci "$(curl -fsSL https://sdk.dfinity.org/install.sh)"
  
 # Copy the installation path you got from the last step and replace <REPLACE WITH YOUR INSTALLATION PATH> from the command below (You can use Notepad for this): export PATH=$PATH:<REPLACE WITH YOUR INSTALLATION PATH>
-export PATH=$PATH:/home/valiantlynx/bin
-export PATH=$PATH:/home/valiantlynx/bin/dfx
+export PATH=$PATH:/home/valiantlynx/bin  # remeber to put it in the bashrc file or zshrc file etc to make it permanent
 
 # Check that it has been added by running
 echo "${PATH//:/$'\n'}"
 
 # Check that dfx has been successfully installed with the following command:
 dfx --version
+dfx upgrade
--- setup_dfx_linux_for_linux_and_macs.sh
+++
@@ -1,2 +0,0 @@
-
-DFX_VERSION=0.9.3 sh -ci "$(curl -fsSL https://sdk.dfinity.org/install.sh)"
